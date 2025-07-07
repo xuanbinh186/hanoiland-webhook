620924b7cf64408157cc85ba88f8d8e8c1694f72
xuanbinh186 <chothuechungcuhn96@gmail.com>
2025-07-08 00:08:45 +0700

    index.js

--- index.js
+++ index.js
@@ -13,6 +13,12 @@ app.post('/webhook', (req, res) => {
 app.get('/', (req, res) => {
   res.send('✅ Webhook Zalo đang hoạt động!');
 });
+const path = yêu cầu('path');
+
+// ✅ Thêm đoạn này để Zalo xác minh domain:
+ứng dụng.lấy('/zalo_verifierHTRkT8JaFdTryAfLaDvzI6klgdU7sNmWCJam.html', (yêu cầu, độ phân giải) => {
+  độ phân giải.gửiTệp(path.giaNhập(__dirname, 'công cộng', 'zalo_verifierHTRkT8JaFdTryAfLaDvzI6klgdU7sNmWCJam.html'));
+});
 
 app.listen(port, () => {
   console.log(`🚀 Server chạy tại cổng ${port}`);
